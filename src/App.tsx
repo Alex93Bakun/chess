a7c12fc1f078fc4555847117735d59f23fe684f8
Alex Bakun <bakun.alex93@gmail.com>
2022-05-18 19:19:38 +0300

    OOP classes, cells init. setup figures. board coordinates. game logic, highlighting available cells. canMove.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { Board } from './models/Board';
+
+import BoardComponent from './components/BoardComponent';
+
 import './App.css';
 
 function App() {
-    return <div className="app">work</div>;
+    const [board, setBoard] = useState(new Board());
+
+    useEffect(() => {
+        restart();
+    }, []);
+
+    function restart() {
+        const newBoard = new Board();
+        newBoard.initCells();
+        newBoard.addFigures();
+        setBoard(newBoard);
+    }
+
+    return (
+        <div className="app">
+            <BoardComponent board={board} setBoard={setBoard} />
+        </div>
+    );
 }
 
 export default App;
